3c23005c7507ada3acc90ce8ed146feaa1a6fe35
veit0r <veit@ludus-venti.de>
2021-12-27 00:08:21 +0100

    collide optimization

---
+++ forest.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="forest" tilewidth="32" tileheight="32" tilecount="3201" columns="97">
+ <properties>
+  <property name="tilesetCopyright" value="CC0 https://opengameart.org/content/hd-multi-layer-parallex-background-samples-of-glitch-game-assets"/>
+ </properties>
+ <image source="tiles_png/glitch_parralax_narroworest_bg2.png" width="3123" height="1087"/>
+</tileset>
